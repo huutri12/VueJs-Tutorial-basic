52976599217293b70469c64e0a5d0abd0f3f9465
huutri12 <tri_1951220112@dau.edu.vn>
2025-05-13 10:58:44 +0700

    done basic

---
+++ 01-syntax/src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+const message = ' hello world'
+const rawHtml = '<span style="color: red">This should be red.</span>'
+const buttonId = 'button-id'
+const buttonClass = 'btn btn-rounded'
+const isButtonDisabled = true
+const objectOfAttrs = {
+  id: 'container',
+  class: 'wrapper'
+}
+const number = 1
+const listString = 'a,b,c,d,e,f,j'
+const attributeName = 'href'
+const url = 'https:kungfutech.edu.vn'
+</script>
+
+<template>
+  <h1>Syntax</h1>
+  <p>Message: {{ message }}</p>
+  <span v-html="rawHtml"></span>
+  <button :id="buttonId" :class="buttonClass" :disabled="isButtonDisabled">Click here</button>
+  <button v-bind="objectOfAttrs">Click Here !!!</button>
+  <button :id="listString">Number: {{ number }}</button>
+  <a v-bind:[attributeName]="url">Click Here</a>
+</template>
---
+++ 05-conditional/src/App.vue
@@ -0,0 +1,41 @@
+<script setup>
+import { ref, reactive } from 'vue'
+const isLoggedIn = ref(true)
+const toggleLogin = () => {
+  isLoggedIn.value = !isLoggedIn
+}
+
+const isVisible = ref(true)
+const toggleShow = () => {
+  isVisible.value = !isVisible
+}
+
+const userStatus = ref('busy')
+
+const changeStatus = () => {
+  if (userStatus.value === 'online') {
+    userStatus.value = 'away'
+  } else if (userStatus.value === 'alway') {
+    userStatus.value = 'busy'
+  } else if (userStatus.value === 'busy') {
+    userStatus.value = 'online'
+  }
+}
+</script>
+
+<template>
+  <div>
+    <h1 v-if="isLoggedIn">Chào mừng người dùng đã đăng nhập</h1>
+    <h1 v-else>Chào mừng khách</h1>
+    <button @click="toggleLogin">{{ isLoggedIn ? 'Đăng xuất' : 'Đăng nhập' }}</button>
+    <br />
+    <h1 v-if="userStatus === 'online'">Người dùng đang online</h1>
+    <h1 v-else-if="userStatus === 'alway'">Người dùng đang vắng mặt</h1>
+    <h1 v-else-if="userStatus === 'busy'">Người dùng đang bận</h1>
+    <h1 v-else>Không rõ trạng thái</h1>
+    <button @click="changeStatus">Thay đổi trạng thái</button>
+    <br />
+    <h1 v-show="isVisible">Nội dung ẩn hoặc hiển thị</h1>
+    <button @click="toggleShow">{{ isVisible ? 'Ẩn' : 'Hiện' }}</button>
+  </div>
+</template>
