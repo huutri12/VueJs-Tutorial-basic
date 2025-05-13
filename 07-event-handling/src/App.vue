25ff96c5c3b9b99750a542aaf9b1ecf878f085eb
huutri12 <tri_1951220112@dau.edu.vn>
2025-05-13 11:11:14 +0700

    done 07

--- 07-event-handling/src/App.vue
+++ 07-event-handling/src/App.vue
@@ -6,14 +6,21 @@ const sayHi1 = () => {
 const sayHi2 = () => {
   alert(`xin chao 2`)
 }
+
+const submit = () => {
+  alert(' Form submited ')
+}
 </script>
 
 <template>
   <div>
     <h1>Event Handing</h1>
-    <button @click="sayHi1">
+    <!-- <button @click="sayHi1">
       <button @click.stop="sayHi2">say hi 2</button>
       say hi 1
-    </button>
+    </button> -->
+    <form @submit.prevent="submit">
+      <button type="submit">Submit</button>
+    </form>
   </div>
 </template>
