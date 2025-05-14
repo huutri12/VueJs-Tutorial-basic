984dcd44c963d1ce6dc21ddf922ebe8480f25b85
huutri12 <tri_1951220112@dau.edu.vn>
2025-05-14 10:32:19 +0700

    done 10

---
+++ 10-lifecycle-hooks/src/views/AboutView.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about">
+    <h1>Count: {{ count }}</h1>
+    <button @click="increment">Increment</button>
+  </div>
+</template>
+
+<script setup>
+import { onBeforeUpdate, onUpdated, ref } from "vue";
+
+const count = ref(0);
+
+const increment = () => {
+  count.value++;
+};  
+
+onBeforeUpdate(() => {
+  console.log("Before Update: ", count.value);
+});
+
+onUpdated (() => {
+  console.log("Updated: ", count.value);
+});
+</script>
